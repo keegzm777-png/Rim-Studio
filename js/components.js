1c099d336700fdf3b796ed5268ff1a3569fd609f
keegzm777-png <keegzm777@gmail.com>
2026-09-11 09:48:35 +0200

    Add files via upload
    commit

--- js/components.js
+++ js/components.js
@@ -3,7 +3,7 @@
  * Update WHATSAPP_NUMBER when the real number is provided.
  */
 
-var WHATSAPP_NUMBER = "27837813889"; // 083 781 3889
+var WHATSAPP_NUMBER = "27810560116"; // +27 81 056 0116
 
 var FACEBOOK_URL = "https://www.facebook.com/share/14sgt9V3BjC/";
 
